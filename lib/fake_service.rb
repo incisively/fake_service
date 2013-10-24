5231ff539455f2c683396a52f33936d73c5eda53
Anton Versal <antonversal@Antons-MacBook-Pro.local>
2013-10-24 15:12:11 +0300

    removed rails support

--- lib/fake_service.rb
+++ lib/fake_service.rb
@@ -1,8 +1,3 @@
-require "fake_service/middleware"
-require "fake_service/server"
-require "fake_service/version"
-require "fake_service/railtie" if defined?(Rails)
-
-module FakeService
-  # Your code goes here...
-end
+require 'fake_service/middleware'
+require 'fake_service/server'
+require 'fake_service/version'
